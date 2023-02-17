3c12053dd040089234f4cd472c9bef4b28a5bbe5
Gabriel Pontel Dal Bó <gabrielpdalbo@gmail.com>
2023-02-16 21:02:48 -0300

    Criando HTML com JavaScript

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,6 +1,18 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: 'maykbrito',
+        name: 'Mayk Brito',
+        public_repos: '76',
+        followers: '120000'
+      }
+    ]
   }
 }
 
@@ -8,17 +20,56 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector('table tbody')
+
     this.update()
   }
 
   update() {
     this.removeAllTr()
+
+    this.entries.forEach(user => {
+      const row = this.createRow()
+
+      row.querySelector(
+        '.user img'
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `
+    <td class="user">
+      <img
+        src="https://github.com/diego3g.png"
+        alt="Imagem de Diego Fernandes"
+      />
+      <a href="https://github.com/diego3g" target="_blank">
+        <p>Diego Fernandes</p>
+        <span>diego3g</span>
+      </a>
+    </td>
+    <td class="repositories">48</td>
+    <td class="followers">22503</td>
+    <td>
+      <button class="remove">&times;</button>
+    </td>
+    `
+
+    return tr
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector('table tbody')
-
-    tbody.querySelectorAll('tr').forEach(tr => {
+    this.tbody.querySelectorAll('tr').forEach(tr => {
       tr.remove()
     })
   }
